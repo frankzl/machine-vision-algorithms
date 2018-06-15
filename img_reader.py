20f8180be6d3eb4629f9e4bf626cb94a9e011e26
Frank Lu <frankz.lu@googlemail.com>
2018-06-15 11:56:47 +0200

    translation

--- img_reader.py
+++ img_reader.py
@@ -1,5 +1,6 @@
 from scipy import misc
 import matplotlib.pyplot as plt
+import numpy as np
 
 def read_img(path):
     """
@@ -10,3 +11,11 @@ def read_img(path):
 def show_img(img):
     plt.imshow(img, cmap=plt.cm.gray)
     plt.show()
+    return
+
+    
+def rgb2gray(rgb):
+    return np.dot(rgb, [0.299, 0.587, 0.114])
+
+def read_gray(path):
+    return rgb2gray(misc.imread(path))
