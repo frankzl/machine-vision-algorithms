ad3b498ab6ad49caef03321785dac0a2697afb3d
Frank Lu <frankz.lu@googlemail.com>
2018-06-18 13:23:14 +0200

    global thresholding

--- img_reader.py
+++ img_reader.py
@@ -25,3 +25,31 @@ def rgb2gray(rgb):
 
 def read_gray(path):
     return rgb2gray(misc.imread(path))
+
+
+def show_histo(img_gray):
+    temp1 = np.rint(img_gray)
+    temp1 = np.reshape(temp1, (-1))
+
+    import collections
+    counter=collections.Counter(temp1)
+    histogram = (list(counter.values()))
+
+    import matplotlib.pyplot as plt
+    plt.plot(histogram)
+    plt.ylabel('some numbers')
+    plt.show()
+
+def get_avg( img, x, y ):
+    suma = 0
+    for i in range(x-2, x+3):
+        for j in range(y-2, y+3):
+            suma += img[i][j]
+    return suma/(5*5)
+
+def get5x5_mean(img_gray):
+    mean_5x5 = np.array(img_gray)
+    for i in range(2, len(mean_5x5) - 2):
+        for j in range(2, len(img_gray[0]) - 2):
+            mean_5x5[i][j] = get_avg( img_gray, i, j )
+    return mean_5x5
